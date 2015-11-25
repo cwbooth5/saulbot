c9a0d1429df0bf2bcef029812b083f4ab8abfacc
Pi <pi+git@pihost.us>
2015-11-25 13:00:20 -0800

    avoid using the default reactor

--- saulbot.py
+++ saulbot.py
@@ -2,8 +2,8 @@
 
 import sys
 from twisted.words.protocols import irc
-from twisted.internet import protocol
-from twisted.internet import reactor
+from twisted.internet import defer, protocol
+from twisted.internet.task import react, deferLater
 
 from pymarkovchain import MarkovChain
 
@@ -24,7 +24,7 @@ class SaulBot(irc.IRCClient):
         if not user or self.nickname not in msg:
             return
         sentence = self.factory.markov.generateString()
-        reactor.callLater(
+        deferLater(self.factory.reactor,
             len(sentence) / 500.0,
             self.msg, target, sentence)
 
@@ -32,10 +32,11 @@ class SaulBot(irc.IRCClient):
 class SaulBotFactory(protocol.ClientFactory):
     protocol = SaulBot
 
-    def __init__(self, channel='', nickname=''):
+    def __init__(self, reactor, channel='', nickname=''):
         self.channel = channel
         self.nickname = nickname
         self.markov = MarkovChain("./tempchain")
+        self.reactor = reactor
 
         with open('corpus.txt', 'r') as f:
             self.markov.generateDatabase(f.read())
@@ -47,18 +48,20 @@ class SaulBotFactory(protocol.ClientFactory):
     def clientConnectionFailed(self, connector, reason):
         print "Could not connect: %s" % (reason,)
 
-
-if __name__ == "__main__":
+def saulbot(reactor, argv)
     try:
-        chan = sys.argv[1]
+        chan = argv[1]
     except IndexError:
         print "Please specify a channel name."
 
     # Allow for testing of the generated quips.
     if chan == 'test':
-        print SaulBotFactory().markov.generateString()
+        print SaulBotFactory(reactor).markov.generateString()
         sys.exit()
 
-    reactor.connectTCP('irc.catch22.org', 6667, SaulBotFactory('#' + chan, 'saulbot'))
+    reactor.connectTCP('irc.catch22.org', 6667, SaulBotFactory(reactor, '#' + chan, 'saulbot'))
+    # Return a deferred that never fires
+    return defer.Deferred()
 
-    reactor.run()
+if __name__ == "__main__":
+    react(main, sys.argv)
